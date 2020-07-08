4d785ae555542af159e06a73d327191dedfd27d1
Partha Sanjoy Dev <parthasanjoydev@gmail.com>
2020-07-08 21:21:00 +0600

    initial commit

---
+++ cachematrix_ProgrammingAssignment2.R
@@ -0,0 +1,33 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## taking a matrix from user, showing the inverse matrix if that is cached.
+
+makeCacheMatrix <- function(x = matrix()) {
+        inv <- NULL
+        set <- function(y){
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() {x}
+        setInverse <- function(inverse) {inv <<- inverse}
+        getInverse <- function() {inv}
+        list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+
+}
+
+
+## complete inversion after getting the matrix.
+
+cacheSolve <- function(x, ...) {
+        inv <- x$getInverse()
+        if(!is.null(inv)){
+                message("getting cached data")
+                return(inv)
+        }
+        mat <- x$get()
+        inv <- solve(mat, ...)
+        x$setInverse(inv)
+        inv
+        ## Return a matrix that is the inverse of 'x'
+}
